Derivatives corrected. Same as numerical derivative.

backprop.py
@@ -1,5 +1,13 @@
 from definitions import *
 
+def predict(self, freq, test_sentence):
+	sl = len(test_sentence)
+	L = self.We[:,test_sentence]
+	tr = tree(sl, self.hiddenSize, self.cat_size, L)
+	tr.forward(freq, self.W1, self.W2, self.W3, self.W4, self.Wcat, self.b1, self.b2, self.b3, self.bcat, self.alpha, self.beta, 0)
+	pred = fcat(np.dot(self.Wcat,tr.nodeFeatures[:,2*sl-2]))
+	return 1*(pred>0.5)
+			
 '''Backpropagation for derivative and cost computation'''
 def backprop(self, training_data, training_labels, freq_original):
 	W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, We = self.W1, self.W2, self.W3, self.W4, self.Wcat, self.b1, self.b2, self.b3, self.bcat, self.We
@@ -7,6 +15,7 @@ def backprop(self, training_data, training_labels, freq_original):
 	sb = (self.hiddenSize, 1)
 	gW1, gW2, gW3, gW4, gWcat, gb1, gb2, gb3, gbcat, gWe = np.zeros(sW), np.zeros(sW), np.zeros(sW), np.zeros(sW), np.zeros((self.cat_size, self.hiddenSize)), np.zeros(sb), np.zeros(sb), np.zeros(sb), np.zeros((self.cat_size,1)), np.zeros((self.hiddenSize, self.dictionary_length))
 	gWe_tot = np.zeros((self.hiddenSize, self.dictionary_length))	
+	cost_J = 0.0
 	for i in range(len(training_data)):
 		word_indices = training_data[i]
 		true_label = training_labels[i]
@@ -20,7 +29,7 @@ def backprop(self, training_data, training_labels, freq_original):
 			for current in range(2*sl-2,sl-1,-1):
 				kid1, kid2 = tr.kids[current,0], tr.kids[current,1]
 				a1, a1_unnorm = tr.nodeFeatures[:,current:current+1], tr.nodeFeatures_unnorm[:,current:current+1]
-				d1, d2 = tr.delta1[:,current:current+1], tr.delta1[:,current:current+1]
+				d1, d2 = tr.delta1[:,current:current+1], tr.delta2[:,current:current+1]
 				pd = tr.parentdelta[:,current:current+1]
 				pp = tr.pp[current]
 				if(current==(2*sl-2)):
@@ -33,7 +42,7 @@ def backprop(self, training_data, training_labels, freq_original):
 				smd = tr.catdelta[:, current:current+1]
 				gbcat += smd
 				h = np.dot(W3.T, d1) + np.dot(W4.T, d2) + np.dot(W.T, pd) + np.dot(Wcat.T, smd) - delt
-				parent_d = np.dot(f_prime(a1_unnorm), h)
+				parent_d = np.dot(fnorm_prime(a1_unnorm), h)
 				gWcat += np.dot(smd,a1.T)
 				tr.parentdelta[:,kid1:kid1+1], tr.parentdelta[:,kid2:kid2+1] = parent_d, parent_d
 				gb1, gb2, gb3 = gb1+parent_d, gb2+d1, gb3+d2
@@ -47,9 +56,21 @@ def backprop(self, training_data, training_labels, freq_original):
 				gbcat += tr.catdelta[:,j]  
 				gL[:,j:j+1] += np.dot(W.T,tr.parentdelta[:,j:j+1]) + np.dot(Wcat.T,tr.catdelta[:,j:j+1]) - delt 	
 				gWe[:,word_indices[j]] += gL[:,j]
-			print gW1[1,1]
-			print tr.checkgradient(freq, 0.000001, W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, self.alpha, self.beta, true_label)
+			cost_J += sum(tr.nodeScores) + sum(tr.nodeScoresR)
+			print gWe[0,2]
+			print tr.checkgradient(word_indices, freq, 0.0000000000001, W1, W2, W3, W4, Wcat, We, b1, b2, b3, bcat, self.alpha, self.beta, true_label)
 		gWe_tot += gWe
+	F = np.ndarray.flatten
+	D = np.dot
+	#final grad computation
+	grad_J = np.concatenate([F(gW1),F(gW2),F(gW3),F(gW4),F(gWcat),F(gb1),F(gb2),F(gb3),F(gbcat),F(gWe_tot)],axis=1)
+	grad_reg = np.concatenate([F(W1),F(W2),F(W3),F(W4),F(Wcat),F(We)],axis=1)
+	grad = np.concatenate([grad_J/len(training_data), .0004*grad_reg],axis=1)	
+	#print grad.shape
+	#final cost computation		
+	cost_reg = .0004*(D(F(W1),F(W1).T)+D(F(W2),F(W2).T)+D(F(W3),F(W3).T)+D(F(W4),F(W4).T)+D(F(Wcat),F(Wcat).T)+D(F(We),F(We).T))
+	cost = cost_J/len(training_data) + cost_reg
+	return (grad, cost)
 		
 		
 

definitions.py
@@ -1,24 +1,29 @@
 import numpy as np
 
 def f(x):
+	f = np.tanh(x)
+	return f
+
+def fnorm(x):
 	(n1, n2) = x.shape
 	f = np.tanh(x)
 	norm = np.linalg.norm(f,axis=0)*np.ones((n1,n2))
 	fnorm = f/norm
-	#return f
 	return fnorm	
 
 def fcat(x):
 	return 1/(1+np.exp(-x))
 
-def f_prime(x):
-	f = x
-	#f = np.tanh(x)
+def f_prime(f):
+	f_p = 1 - np.square(f)
+	return f_p
+
+def fnorm_prime(f_unnorm):
+	f = f_unnorm
 	f_p = 1 - np.square(f)
 	diag = np.diagflat(f_p)
 	norm = np.linalg.norm(f)
 	fnorm_p = diag/norm - np.dot(diag, np.dot(f, f.T))/np.power(norm,3)
-	#return f_p
 	return fnorm_p
 
 def fcat_prime(x):
@@ -49,77 +54,89 @@ class tree:
 
 	def forward(self, freq, W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, alpha, beta, sentence_label):
 		sl = self.sl
+		D = np.dot
 		'''Builds tree and computes recontruction error for each node'''
 		words = self.words
 		for j in range(0,sl-1):
 			lens = words.shape[1]
-			c1, f1, c2, f2 = words[:,0:lens-1], freq[0:lens-1], words[:,1:lens], freq[1:lens]
-			hidden_out = f(np.dot(W1,c1)+np.dot(W2,c2)+np.tile(b1,lens-1))		
-			hidden_out_norm = hidden_out #only for normalized tanh
-			y1, y2 = f(np.dot(W3,hidden_out_norm)+np.tile(b2,lens-1)), f(np.dot(W4,hidden_out_norm)+np.tile(b3,lens-1))	
-			y1_norm, y2_norm = y1, y2 #only for normalized tanh
-			y1c1, y2c2 = alpha*(y1-c1), alpha*(y2-c2)
-			recons_error = sum(y1c1*(y1-c1)+y2c2*(y2-c2))*0.5
-			re_min, re_min_pos = np.min(recons_error), np.argmin(recons_error)
-			self.y1c1[:,sl+j], self.y2c2[:,sl+j] = y1c1[:,re_min_pos], y2c2[:,re_min_pos]
-			self.delta1[:,sl+j], self.delta2[:,sl+j] = np.dot(f_prime(y1[:,re_min_pos]), y1c1[:,re_min_pos]), np.dot(f_prime(y2[:,re_min_pos]), y2c2[:,re_min_pos])			
-			index_child1, index_child2 = self.collapsed[re_min_pos], self.collapsed[re_min_pos+1]
-			words = np.delete(words,re_min_pos+1,1)
-			words[:,re_min_pos] = hidden_out_norm[:,re_min_pos]
-			self.nodeFeatures[:,sl+j], self.nodeFeatures_unnorm[:,sl+j] = hidden_out_norm[:,re_min_pos], hidden_out[:,re_min_pos]
-			self.nodeScoresR[sl+j] = re_min
+			c1, f1, c2, f2 = words[:,0:lens-1], freq[0:lens-1], words[:,1:lens], freq[1:lens]	
+			p = f(D(W1,c1)+D(W2,c2)+np.tile(b1,lens-1))		
+			p_norm = p/(np.linalg.norm(p,axis=0)*np.ones(p.shape))
+			y1, y2 = f(D(W3,p_norm)+np.tile(b2,lens-1)), f(D(W4,p_norm)+np.tile(b3,lens-1))	
+			y1_norm, y2_norm = y1/(np.linalg.norm(y1,axis=0)*np.ones(y1.shape)), y2/(np.linalg.norm(y2,axis=0)*np.ones(y2.shape))	
+			y1c1, y2c2 = alpha*(y1_norm-c1), alpha*(y2_norm-c2)
+			recons_error = sum(y1c1*(y1_norm-c1)+y2c2*(y2_norm-c2))*0.5
+			m, mp = np.min(recons_error), np.argmin(recons_error)
+			self.y1c1[:,sl+j], self.y2c2[:,sl+j] = y1c1[:,mp], y2c2[:,mp]
+			self.delta1[:,sl+j:sl+j+1], self.delta2[:,sl+j:sl+j+1] = D(fnorm_prime(y1[:,mp:mp+1]), y1c1[:,mp:mp+1]), D(fnorm_prime(y2[:,mp:mp+1]), y2c2[:,mp:mp+1])			
+			index_child1, index_child2 = self.collapsed[mp], self.collapsed[mp+1]
+			words = np.delete(words,mp+1,1)
+			words[:,mp] = p_norm[:,mp]
+			self.nodeFeatures[:,sl+j], self.nodeFeatures_unnorm[:,sl+j] = p_norm[:,mp], p[:,mp]
+			self.nodeScoresR[sl+j] = m
 			self.pp[index_child1], self.pp[index_child2] = sl+j, sl+j
 			self.kids[sl+j,0], self.kids[sl+j,1] = index_child1, index_child2
 			self.numkids[sl+j] = self.numkids[self.kids[sl+j,0]] + self.numkids[self.kids[sl+j,1]]
-			self.freq = np.delete(self.freq,re_min_pos+1,0) 
-			self.freq[re_min_pos] = (np.dot(self.numkids[self.kids[sl+j,0]], f1[re_min_pos]) +  np.dot(self.numkids[self.kids[sl+j,1]], f2[re_min_pos]))/self.numkids[sl+j]
-			del self.collapsed[re_min_pos]
-			self.collapsed[re_min_pos]=sl+j
+			self.freq = np.delete(self.freq,mp+1,0) 
+			self.freq[mp] = (D(self.numkids[self.kids[sl+j,0]], f1[mp]) +  D(self.numkids[self.kids[sl+j,1]], f2[mp]))/self.numkids[sl+j]
+			del self.collapsed[mp]
+			self.collapsed[mp]=sl+j
 		'''Classification error computation for each node'''
-		out = fcat(np.dot(Wcat,self.words)+np.tile(bcat,sl))
+		out = fcat(D(Wcat,self.words)+np.tile(bcat,sl))
 		diff = np.tile(sentence_label,sl)-out
 		lbl_sm = (1-alpha)*diff
 		score = 0.5*lbl_sm*diff
 		self.nodeScores[0:sl], self.catdelta[:,0:sl] = score.T, -(lbl_sm)*fcat_prime(out)
 		for i in range(sl,2*sl-1):
-			sm = fcat(np.dot(Wcat,self.nodeFeatures[:,i]) + bcat)
+			sm = fcat(D(Wcat,self.nodeFeatures[:,i]) + bcat)
 			lbl_sm = beta*(1-alpha)*(sentence_label-sm)
 			self.catdelta[:,i] = -(lbl_sm)*fcat_prime(sm)
-			J = 0.5*(np.dot(lbl_sm.T,(sentence_label-sm)))
+			J = 0.5*(D(lbl_sm.T,(sentence_label-sm)))
 			self.nodeScores[i] = J
 	
-	def cost(self, w, W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, alpha, beta, sentence_label):
-		words = w.copy()
+	def cost(self, words, W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, alpha, beta, sentence_label):
+		D = np.dot
 		sl = self.sl
 		nodeScoresR = np.zeros((2*sl-1,1))		
 		nodeScores = np.zeros((2*sl-1,1))		
 		nF = self.nodeFeatures.copy()
-		nF[:,0:sl] = w
+		nF[:,0:sl] = words
 		for j in range(0,sl-1):
 			k1, k2 = self.kids[sl+j,0], self.kids[sl+j,1]
 			c1, c2 = nF[:,k1:k1+1], nF[:,k2:k2+1]
-			nF[:,j:j+1] = f(np.dot(W1,c1)+np.dot(W2,c2)+b1)
-			y1, y2 = f(np.dot(W3,nF[:,j:j+1])+b2), f(np.dot(W4,nF[:,j:j+1])+b3)
-			y1c1, y2c2 = alpha*(y1-c1), alpha*(y2-c2)
-			nodeScoresR[sl+j] = sum(y1c1*(y1-c1)+y2c2*(y2-c2))*0.5
-		out = fcat(np.dot(Wcat,words)+np.tile(bcat,sl))
+			nF[:,sl+j:sl+j+1] = fnorm(D(W1,c1)+D(W2,c2)+b1)
+			y1, y2 = f(D(W3,nF[:,sl+j:sl+j+1])+b2), f(D(W4,nF[:,sl+j:sl+j+1])+b3)
+			y1_norm, y2_norm = y1/(np.linalg.norm(y1,axis=0)*np.ones(y1.shape)), y2/(np.linalg.norm(y2,axis=0)*np.ones(y2.shape))	
+			y1c1, y2c2 = alpha*(y1_norm-c1), alpha*(y2_norm-c2)
+			nodeScoresR[sl+j] = sum(y1c1*(y1_norm-c1)+y2c2*(y2_norm-c2))*0.5
+		out = fcat(D(Wcat,words)+np.tile(bcat,sl))
 		diff = np.tile(sentence_label,sl)-out
 		lbl_sm = (1-alpha)*diff
 		score = 0.5*lbl_sm*diff
 		nodeScores[0:sl] = score.T
 		for i in range(sl,2*sl-1):
-			sm = fcat(np.dot(Wcat,nF[:,i]) + bcat)
+			sm = fcat(D(Wcat,nF[:,i]) + bcat)
 			lbl_sm = beta*(1-alpha)*(sentence_label-sm)
-			nodeScores[i] = 0.5*(np.dot(lbl_sm.T,(sentence_label-sm)))
+			nodeScores[i] = 0.5*(D(lbl_sm.T,(sentence_label-sm)))
 		error = (sum(nodeScoresR) + sum(nodeScores))
 		return error
 
-	def checkgradient(self, freq, eps, W1, W2, W3, W4, Wcat, b1, b2, b3, bcat, alpha, beta, sl):
-		w = self.words.copy()
+	def checkgradient(self, sentence, freq, eps, W1, W2, W3, W4, Wcat, We, b1, b2, b3, bcat, alpha, beta, sl):
+		w = We[:,sentence]
+		wa, wb = w.copy(), w.copy()
 		W1a, W1b = W1.copy(), W1.copy()
-		W1a[1,1], W1b[1,1] = W1[1,1] + eps, W1[1,1] - eps
-		j1 = self.cost(w,W1a,W2,W3,W4,Wcat,b1,b2,b3,bcat,alpha,beta,sl)
-		j2 = self.cost(w,W1b,W2,W3,W4,Wcat,b1,b2,b3,bcat,alpha,beta,sl)
+		W2a, W2b = W2.copy(), W2.copy()
+		W3a, W3b = W3.copy(), W3.copy()
+		W4a, W4b = W4.copy(), W4.copy()
+		Wcata, Wcatb = Wcat.copy(), Wcat.copy()
+		#W2a[3,3], W2b[3,3] = W2[3,3] + eps, W2[3,3] - eps
+		#W1a[3,3], W1b[3,3] = W1[3,3] + eps, W1[3,3] - eps
+		#W3a[3,4], W3b[3,4] = W3[3,4] + eps, W3[3,4] - eps
+		#W4a[2,4], W4b[2,4] = W4[2,4] + eps, W4[2,4] - eps
+		#Wcata[0,0], Wcatb[0,0] = Wcat[0,0] + eps, Wcat[0,0] - eps
+		wa[0,2], wb[0,2] = w[0,2] + eps, w[0,2] - eps	
+		j1 = self.cost(wa,W1a,W2a,W3a,W4a,Wcata,b1,b2,b3,bcat,alpha,beta,sl)
+		j2 = self.cost(wb,W1b,W2b,W3b,W4b,Wcatb,b1,b2,b3,bcat,alpha,beta,sl)
 		return (j1-j2)/(2*eps)
 
 

helper.py
@@ -1,6 +1,6 @@
 #from def2 import *
 from backprop import *
-from scipy import io as sio
+import scipy.io
 import math
 
 class parameters:
@@ -21,8 +21,16 @@ class parameters:
 		self.dictionary_length = dictionary_length
 		self.alpha = alpha
 		self.beta = beta
-	updateparams = backprop
+	computeder = backprop
+	predict = predict
 
+
+def accuracy(test_sentences, labels, freq, p):
+	n = len(test_sentences)
+	correct = 0
+	for i in range(len(test_sentences)):
+		correct += 1*(p.predict(freq,test_sentences[i])==labels[i])
+	return correct/float(n)
 	
 def loaddata(path):
 	vocab = sio.loadmat(path+'vocab.mat', squeeze_me=True, struct_as_record=False)['words'] #vocabulary used 
@@ -44,6 +52,30 @@ def loaddata(path):
 	allSNum[6549] = 20143;
 	allSStr[9800] = [u'crummy'];
 	allSNum[9800] = 241211;
+
+
+def load():
+	data = scipy.io.loadmat('data.mat')
+	data = data['snum']
+	train = []
+	for i in range(data.shape[0]):
+		x = data[i]
+		y = x[0]
+		z = y.tolist()
+		u = z[0]
+		train.append(u)
+
+	lbl = scipy.io.loadmat('labels.mat')
+	lbl = lbl['lbl']
+	lbl = lbl[0]
+	labels = []
+	for i in range(lbl.shape[0]):
+		labels.append(lbl[i])	
+	return (train, labels)
+
+
+
+
 
 
 

main.py
@@ -1,14 +1,20 @@
 from helper import *
-
+import scipy.io
 #loaddata("codeDataMoviesEMNLP/data/rt-polaritydata/")
 #vocab = 10
-freq = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-data = [[1,2,3], [3,4,5], [1,3,9], [2,4,2]]
-labels = [0, 0, 1, 1]
+
+(train, labels) = load()
+#freq = [1/float(14043)]*14043
+freq = [0.1]*10
+train2 = [[1,2,3], [3,4,5], [1,3,9], [2,4,2]]
+labels2 = [0, 0, 1, 1]
 hiddenSize = 5
 p = parameters(hiddenSize,hiddenSize,1,10,0.2,0.5)
 #p.updateparams(0, data, labels, freq)
 #p.updateparams(1, data, labels, freq)
-p.updateparams(data, labels, freq)
+(x, y) = p.computeder(train2, labels2, freq)
+#print x.shape
+#print y
+#print accuracy(train2, labels2, freq, p)
 
 