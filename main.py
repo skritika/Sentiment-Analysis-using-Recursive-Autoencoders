ae7030f3904b79f5c6b26f8b01099bd696e141da
skritika <kritikasingh@gmail.com>
2014-03-16 14:55:39 -0700

    Randomized training data

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from scipy.optimize import fmin_l_bfgs_b
 #loaddata("codeDataMoviesEMNLP/data/rt-polaritydata/")
 #vocab = 10
 
-(train, labels) = load()
 #freq = [1/float(14043)]*14043
-hiddenSize = 5
 #p = parameters(hiddenSize,hiddenSize,1,10,0.2,0.5)
 #p.updateparams(0, data, labels, freq)
 #p.updateparams(1, data, labels, freq)
@@ -14,18 +12,26 @@ hiddenSize = 5
 #print x.shape
 #print y
 #print accuracy(train2, labels2, freq, p)
-initv = init_theta(5,1,10)
+#train2 = [[1,2,3], [3,4,5], [1,3,9], [2,4,2]]
+#labels2 = [0, 0, 1, 1]
 
 
 
 d = 20
-#train2 = [[1,2,3], [3,4,5], [1,3,9], [2,4,2]]
-#labels2 = [0, 0, 1, 1]
+(t, l) = load()
+s = range(len(t))
+np.random.shuffle(s)
+s = s[0:100]
+train = [t[i] for i in s]
+labels = [l[i] for i in s]
+
+print len(train)
+print len(labels)
 num_cat = 1
-#dict_length = 10
 dict_length = 14043
 alpha = 0.2
 beta = 0.5
+initv = init_theta(d,num_cat,dict_length)
 freq = [1/float(14043)]*14043
 fgrad = partial(backprop, 1, train, labels, freq, d, num_cat, dict_length, alpha, beta)
 fcost = partial(backprop, 0, train, labels, freq, d, num_cat, dict_length, alpha, beta)
@@ -35,7 +41,7 @@ fcost = partial(backprop, 0, train, labels, freq, d, num_cat, dict_length, alpha
 
 theta_min = fmin_l_bfgs_b(fcost, initv, fprime = fgrad)[0] 
 (W1,W2,W3,W4,Wcat,We,b1,b2,b3,bcat) = getW(theta_min, d, num_cat, dict_length)
-test = train2
-test_labels = labels2
+test = train
+test_labels = labels
 print accuracy(W1, W2, W3, W4, Wcat, We, b1, b2, b3, bcat, alpha, beta, freq, test, test_labels, d, num_cat)
 
